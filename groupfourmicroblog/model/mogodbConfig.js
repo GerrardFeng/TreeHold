af43eeebfe827d6ac0f8fb11092c7cd861b317b8
FENGGE <gerrard.feng@oocl.com>
2015-05-19 21:57:25 +0800

    Revert "MEAN Architecture"
    This reverts commit a35785d130b2c32d0a5d9fea11f5e61f349023a1.

--- groupfourmicroblog/model/mogodbConfig.js
+++
@@ -1,8 +0,0 @@
-
-function mongodbConfig(){
-    return {db : "fengge-w7:27017/nodetest1",
-            host : "fengge-w7:27017",
-            dbname : "nodetest1"};
-}
-
-exports.mongodbConfigs = mongodbConfig;
